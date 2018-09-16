c9617669fed17e31b7313cd95eef147fd3ece8d0
Elaheh Gholizadeh <egholizadeh@tyro.com>
2018-09-16 14:20:25 +1000

    cleanup

--- scripts/apply.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -xe
-
-echo "+++ :terraform: Terraform Init"
-terraform init \
-    -backend-config="bucket=terraform-tfstate-elaheh"
-echo "+++ :terraform: Terraform Apply"
-terraform apply tfplan-test
